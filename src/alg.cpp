1b84217fa99c8d57de1c2fe74a47aba2205eafd5
kocherovaarina <arinakocerova@gmail.com>
2026-03-14 12:17:32 +0300

    Task done by Kocherova Arina from 25 IST 4-2

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,17 +1,48 @@
 // Copyright 2025 NNTU-CS
+#include <iostream>
 #include <cstdint>
 #include "alg.h"
 
-uint64_t collatzMaxValue(uint64_t num) {
-  return 1;
-}
-
 unsigned int collatzLen(uint64_t num) {
-  return 1;
+  unsigned int length = 0;
+  while (num != 1) {
+    if (num % 2 == 0) {
+      num /= 2;
+    }
+    else {
+      num = 3 * num + 1;
+    }
+    length++;
+  }
+  return length + 1;
+}
+uint64_t collatzMaxValue(uint64_t num) {
+  uint64_t maxValue = num;
+  while (num != 1) {
+    if (num % 2 == 0) {
+      num /= 2;
+    }
+    else {
+      num = 3 * num + 1;
+    }
+    if (num > maxValue) {
+      maxValue = num;
+    }
+  }
+  return maxValue;
 }
 
-unsigned int seqCollatz(unsigned int *maxlen,
-                        uint64_t lbound,
-                        uint64_t rbound) {
-  return 1;
+unsigned int seqCollatz(unsigned int *maxlen, uint64_t lbound, uint64_t rbound) {
+  unsigned int longestLength = 0;
+  uint64_t numberWithLongestLength = lbound;
+  for (uint64_t i = lbound; i <= rbound; ++i) {
+    unsigned int currentLength = collatzLen(i);
+    if (currentLength > longestLength) {
+      longestLength = currentLength;
+      numberWithLongestLength = i;
+    }
+  }
+  
+  *maxlen = longestLength;
+  return numberWithLongestLength;
 }
